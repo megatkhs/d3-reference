42cf0f5876e6121ea7402fe5f158d29372560518
megatkhs <tarodino0526@gmail.com>
2018-01-11 01:13:23 +0900

    変更 gulp関連

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ let gulp = require('gulp'),
     pug = require('gulp-pug'),
     sass = require('gulp-sass'),
     changed = require('gulp-changed'),
-    connect = require('gulp-notify'),
+    connect = require('gulp-connect'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify'),
     browserSync = require('browser-sync'),
     path = require('path');
 
 let src = {
   'root': './src',
-  'pug': './src/pug/*.pug',
-  'scss': './src/sass/*.scss'
+  'pug': './src/pug/**/*.pug',
+  'scss': './src/scss/**/*.scss'
 };
 
 gulp.task('default', ['server_start', 'watch_files']);
@@ -25,4 +27,52 @@ gulp.task('server_start', function(){
 gulp.task('watch_files', function(){
   gulp.watch(src.pug, ['change_pug']);
   gulp.watch(src.scss, ['change_scss']);
-})
+});
+
+// *.pugが更新されたときの動作
+gulp.task('change_pug', function(){
+  gulp.src(src.pug)
+      .pipe(plumber({
+        errorHandler: notify.onError('Error: <%= error.message %>')
+      }))
+      .pipe(pug({
+        pretty: true
+      }))
+      .pipe(gulp.dest('./'))
+      .pipe(connect.reload());
+      console.log('pugをコンパイルしました');
+});
+
+// *.scssが更新されたときの動作
+gulp.task('change_scss', function(){
+  gulp.src(src.scss)
+      .pipe(plumber({
+        errorHandler: notify.onError('Error: <%= error.message %>')
+      }))
+      .pipe(sass({
+        pretty: 'expanded'
+      }))
+      .pipe(gulp.dest('./css'))
+      .pipe(connect.reload());
+});
+
+gulp.task('all_build', function(){
+  gulp.src([src.pug, '!'+src.pug])
+      .pipe(plumber({
+        errorHandler: notify.onError('Error: <%= error.message %>')
+      }))
+      .pipe(pug({
+        basedir: src.root,
+        pretty: true
+      }))
+      .pipe(gulp.dest('./'));
+
+  gulp.task(src.scss)
+      .pipe(plumber({
+        errorHandler: notify.onError('Error: <%= error.message %>')
+      }))
+      .pipe(scss({
+        pretty: 'expanded'
+      }))
+      .pipe(gulp.dest('./css'));
+});
